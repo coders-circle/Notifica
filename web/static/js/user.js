e5342c869f8f0afb6405739294a3f691a517afe7
Frozen Helium <fren.ankit@gmail.com>
2015-12-14 08:27:28 +0545

    add different button for hide/unhide comments

--- web/static/js/user.js
+++ web/static/js/user.js
@@ -1,9 +1,22 @@
 jQuery(document).ready(function($) {
     $('body').on( 'click', '.expand-button', function(e){
-        $(this).parent().parent().find(".list-comment").fadeIn();
+        var list_comment = $(this).parent().parent().find(".list-comment");
+        var chevron = list_comment.parent().find("i");
+        if(list_comment.data("hidden") == "false"){
+            list_comment.fadeOut();
+            list_comment.data("hidden", "true");
+            chevron.removeClass("fa-chevron-up");
+            chevron.addClass("fa-chevron-down");
+        }else{
+            list_comment.fadeIn();
+            list_comment.data("hidden", "false");
+            chevron.removeClass("fa-chevron-down");
+            chevron.addClass("fa-chevron-up");
+        }
     });
     $('body').on( 'click', '.card-post', function(e){
-        $(this).find(".list-comment").fadeIn();
+        //$(this).find(".list-comment").fadeIn();
+
     });
 
 });
