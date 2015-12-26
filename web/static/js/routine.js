856b7bc06f835b7c2f981cbccab32eeb77adde2f
Frozen Helium <fren.ankit@gmail.com>
2015-12-26 18:42:28 +0545

    implement dynamic suggestion in modal

--- web/static/js/routine.js
+++ web/static/js/routine.js
@@ -1,16 +1,87 @@
 jQuery(document).ready(function($) {
+	$('#subject-input').selectize({
+		placeholder: 'Subject',
+        valueField: 'name',
+        labelField: 'name',
+        searchField: ['name'],
+        sortField: 'name',
+        create: true,
+		maxItems: 1,
+        render: {
+            option: function(item, escape) {
+				return '<div class="li-suggestion">' +
+                    (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
+                '</div>';
+            }
+        },
+
+        load: function(query, callback) {
+            if (!query.length) return callback();
+            $.ajax({
+                url: '/classroom/api/v1/subjects/?format=json&q=' + encodeURIComponent(query),
+                type: 'GET',
+                error: function() {
+                    error="cant fetch the suggestions";
+                    callback(error);
+                },
+                success: function(res) {
+                    callback(res.slice(0, 10));
+                }
+            });
+        }
+    });
+
+	$('#teachers-input').selectize({
+		placeholder: 'Teachers',
+        valueField: 'email',
+        labelField: 'username',
+        searchField: ['username', 'first_name', 'last_name', 'email'],
+        sortField: 'username',
+        create: true,
+        render: {
+            option: function(item, escape) {
+				return '<div class="li-suggestion">' +
+                    (item.username ? '<span class="username">' + escape(item.username) + '</span>' : '') +
+                    '<br>'+
+                    (item.first_name ? '<span class="name">' + escape(item.first_name) + '&nbsp;</span>' : '') +
+                    (item.last_name ? '<span class="name">' + escape(item.last_name) + '</span>' : '') +
+                    (item.first_name || item.last_name? '<br>':'') +
+                    (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+                '</div>';
+            }
+        },
+
+        load: function(query, callback) {
+            if (!query.length) return callback();
+            $.ajax({
+                url: '/classroom/api/v1/users/?format=json&q=' + encodeURIComponent(query),
+                type: 'GET',
+                error: function() {
+                    error="cant fetch the suggestions";
+                    callback(error);
+                },
+                success: function(res) {
+                    callback(res.slice(0, 10));
+                }
+            });
+        }
+    });
+
+
+
 	$('body').on( 'click', '.btn-add-period', function(e){
-		$('.backdrop').fadeIn();
-		var add_subject_dialog = $('body').find(".add-subject-dialog");
-		add_subject_dialog.find(".input-subject").val("");
-		add_subject_dialog.find(".input-teachers").val("");
-		add_subject_dialog.find(".input-remarks").val("");
-		add_subject_dialog.data("period-container", $(this).parent().parent().find('.periods'));
-		add_subject_dialog.data("new-period", true);
-		setTimeout(function(){
-			add_subject_dialog.addClass("shown");
-			add_subject_dialog.show();
-		}, 200);
+		//$('.backdrop').fadeIn();
+		var add_subject_dialog = $('body').find("#add-subject-dialog");
+		add_subject_dialog.modal('show');
+		//add_subject_dialog.find(".input-subject").val("");
+		//add_subject_dialog.find(".input-teachers").val("");
+		//add_subject_dialog.find(".input-remarks").val("");
+		//add_subject_dialog.data("period-container", $(this).parent().parent().find('.periods'));
+		//add_subject_dialog.data("new-period", true);
+		// setTimeout(function(){
+		// 	add_subject_dialog.addClass("shown");
+		// 	add_subject_dialog.show();
+		// }, 200);
 
 
         // var period_template = $('.template-period').clone();
