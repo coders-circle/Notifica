0f1026311d70658208387285e2ed5e0b400cddf5
Frozen Helium <fren.ankit@gmail.com>
2015-12-26 19:45:45 +0545

    sync

--- web/static/js/routine.js
+++ web/static/js/routine.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
-	$('#subject-input').selectize({
+
+	var $subject_select = $('#subject-input').selectize({
 		placeholder: 'Subject',
         valueField: 'name',
         labelField: 'name',
@@ -31,7 +32,7 @@ jQuery(document).ready(function($) {
         }
     });
 
-	$('#teachers-input').selectize({
+	var $teachers_select = $('#teachers-input').selectize({
 		placeholder: 'Teachers',
         valueField: 'email',
         labelField: 'username',
@@ -67,12 +68,27 @@ jQuery(document).ready(function($) {
         }
     });
 
-
+	var teachers_control = $teachers_select[0].selectize;
+	var subject_control = $subject_select[0].selectize;
 
 	$('body').on( 'click', '.btn-add-period', function(e){
 		//$('.backdrop').fadeIn();
 		var add_subject_dialog = $('body').find("#add-subject-dialog");
 		add_subject_dialog.modal('show');
+		var start_time = add_subject_dialog.find(".input-start-time");
+		var end_time = add_subject_dialog.find(".input-end-time");
+		var subject = add_subject_dialog.find("#subject-input");
+		var teachers = add_subject_dialog.find("#teachers-input");
+		var remarks = add_subject_dialog.find(".input-remarks");
+
+		start_time.val("");
+		end_time.val("");
+		subject.val("");
+		teachers.val("");
+		remarks.val("");
+		teachers_control.clear();
+		subject_control.clear();
+
 		//add_subject_dialog.find(".input-subject").val("");
 		//add_subject_dialog.find(".input-teachers").val("");
 		//add_subject_dialog.find(".input-remarks").val("");
@@ -95,72 +111,13 @@ jQuery(document).ready(function($) {
         // period_template.appendTo($(this).parent().parent().find('.periods'));
     });
 	$('body').on( 'click', '.period', function(e){
-		$('.backdrop').fadeIn();
-		var add_subject_dialog = $('body').find(".add-subject-dialog");
-		add_subject_dialog.data("period", $(this));
-		add_subject_dialog.data("new-period", false);
-		add_subject_dialog.find(".input-subject").val($(this).find(".subject").text());
-		add_subject_dialog.find(".input-teachers").val($(this).find(".teachers").text());
-		add_subject_dialog.find(".input-remarks").val($(this).find(".remarks").text());
-		setTimeout(function(){
-			add_subject_dialog.addClass("shown");
-			add_subject_dialog.show();
-		}, 200);
 	});
 	$('body').on( 'click', '.btn-dlg-ok', function(e){
-		var add_subject_dialog = $(this).parent().parent();
-
-		var subject_input = add_subject_dialog.find(".input-subject");
-		var teachers_input = add_subject_dialog.find(".input-teachers");
-		var remarks_input = add_subject_dialog.find(".input-remarks");
-
-		if(add_subject_dialog.data("new-period")){
-			var period_template = $('.template-period').clone();
-	        period_template.addClass('period');
-	        period_template.removeClass('template-period');
-	        period_template.removeClass('hidden');
-			var subject = period_template.find('.subject');
-			var teachers = period_template.find('.teachers');
-			var remarks = period_template.find('.remarks');
-			subject.text(subject_input.val());
-			teachers.text(teachers_input.val());
-			remarks.text(remarks_input.val());
-			period_template.appendTo(add_subject_dialog.data("period-container"));
-			if(remarks_input.val().length > 0){
-				remarks.show();
-			}else{
-				remarks.hide();
-			}
-		}else{
-			var period = add_subject_dialog.data("period");
-			period.find(".subject").text(subject_input.val());
-			period.find(".teachers").text(teachers_input.val());
-			period.find(".remarks").text(remarks_input.val());
-			if(remarks_input.val().length > 0){
-				period.find(".remarks").show();
-			}else{
-				period.find(".remarks").hide();
-			}
-
-		}
-
-		add_subject_dialog.removeClass("shown");
-		add_subject_dialog.hide();
-		$('.backdrop').fadeOut();
+		var add_subject_dialog = $('body').find("#add-subject-dialog");
+		add_subject_dialog.modal('hide');
 	});
 	$('body').on( 'click', '.btn-dlg-cancel', function(e){
-		var add_subject_dialog = $(this).parent().parent();
-		add_subject_dialog.removeClass("shown");
-		add_subject_dialog.hide();
-		$('.backdrop').fadeOut();
+		var add_subject_dialog = $('body').find("#add-subject-dialog");
+		add_subject_dialog.modal('hide');
 	});
-});
-
-$(document).mouseup(function (e){
-	var container = $(".shown");
-	if (!container.is(e.target) && container.has(e.target).length === 0) {
-		container.removeClass("shown");
-		container.hide();
-		$('.backdrop').fadeOut();
-	}
 });
