aae7919966edd4a000347f4b0f4f74b7ad59d80b
Frozen Helium <fren.ankit@gmail.com>
2015-12-31 20:14:25 +0545

    improve design

--- web/main/views.py
+++ web/main/views.py
@@ -59,56 +59,75 @@ class RegisterView(View):
 
 # TODO: Replace user info with User class
 
-class Comment(object):
-    userName = None
-    userProfileLink = "#"
-    userAvatar = "ninja.png"
-    content = None;
-    time = "few seconds ago"
 
 class UserPost(object):
     userName = None
     userProfileLink = "#"
     userAvatar = "ninja.png"
+    title = None
     content = None
     time = "few seconds ago";
+    numComments = 0
     def __init__(self):
-        self.comments = []
+        self.tags = []
 
 
 class UserView(View):
     def get(self, request):
         if isValidUser(request.user):
-            testComment1 = Comment()
-            testComment1.userName = "fhx"
-            testComment1.content = "hello world"
-
-            testComment2 = Comment()
-            testComment2.userName = "Aditya Khatri"
-            testComment2.content = "OK DOOD!"
-
-            testComment3 = Comment()
-            testComment3.userName = "Bibek Dahal"
-            testComment3.content = "Test Comment"
-
             testPost1 = UserPost()
-            testPost1.userName = "Absolute Zero";
-            testPost1.content = "post post post";
-            testPost1.time = "few minutes ago";
+            testPost1.userName = "Absolute Zero"
+            testPost1.title = "Test post, indeed"
             testPost1.userAvatar = "fh.png"
+            testPost1.tags.append("awesome")
+            testPost1.tags.append("test")
+            testPost1.tags.append("cool")
+            testPost1.tags.append("yolo")
+            testPost1.numComments = 10
 
             testPost2 = UserPost()
             testPost2.userName = "Aditya Khatri"
-            testPost2.content = "OK DOOD!"
+            testPost2.title = "Test post returns"
             testPost2.userAvatar = "ks.png"
+            testPost2.tags.append("noobness")
+            testPost2.tags.append("lazy")
 
-            testPost1.comments.append(testComment1)
-            testPost1.comments.append(testComment2)
-            testPost2.comments.append(testComment3)
+            testPost3 = UserPost()
+            testPost3.userName = "Bibek Dahal"
+            testPost3.title = "Rectify regex search error in REST api"
+            testPost3.userAvatar = "ninja.png"
+            testPost3.tags.append("commit")
+            testPost3.tags.append("github")
+            testPost3.tags.append("notifica")
+
+            testPost4 = UserPost()
+            testPost4.userName = "Bibek Dahal"
+            testPost4.title = "Move user view to main app. Add some teachers"
+            testPost4.userAvatar = "ninja.png"
+            testPost4.tags.append("commit")
+            testPost4.tags.append("github")
+            testPost4.tags.append("notifica")
+
+            testPost5 = UserPost()
+            testPost5.userName = "Absolute Zero"
+            testPost5.title = "fix a bug that caused object skipping in period processing"
+            testPost5.userAvatar = "fh.png"
+            testPost5.tags.append("commit")
+            testPost5.tags.append("github")
+            testPost5.tags.append("notifica")
+
+
 
             userPosts = []
             userPosts.append(testPost1)
             userPosts.append(testPost2)
+            userPosts.append(testPost3)
+            userPosts.append(testPost4)
+            userPosts.append(testPost5)
+            userPosts.append(testPost2)
+            userPosts.append(testPost3)
+            userPosts.append(testPost1)
+            userPosts.append(testPost4)
 
             context = {'userposts':userPosts}
             return render(request, "main/user.html", context)
@@ -117,4 +136,3 @@ class UserView(View):
     def post(self, request):
         logout(request)
         return redirect("home")
-
