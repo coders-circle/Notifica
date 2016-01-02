aa5728341717054d2a2b12a89083136b4e1230d8
bibek <bibekdahal.bd16@gmail.com>
2016-01-02 14:32:40 +0545

    Implement posts search

--- web/posts/rest_views.py
+++ web/posts/rest_views.py
@@ -1,9 +1,12 @@
+from django.db.models import Q
+
 from rest_framework import viewsets
 from rest_framework import permissions
 
 from posts.models import *
 from posts.serializers import *
 
+from main.search import *
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
@@ -13,7 +16,13 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save(posted_by=self.request.user)
 
     def get_queryset(self):
-        return Post.objects.all()
+        searchstring = self.request.GET.get("q")
+        if searchstring and searchstring != "":
+            regexstring = get_regex(searchstring)
+            queryset = Post.objects.filter(Q(title__iregex=regexstring) | Q(body__iregex=regexstring) | Q(tags__iregex=regexstring))
+        else:
+            queryset = Post.objects.all()
+        return queryset
 
 
 class CommentViewSet(viewsets.ModelViewSet):
