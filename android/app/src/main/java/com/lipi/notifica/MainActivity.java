2c903e7bab5b5590718144c5b931904396d560ee
Frozen Helium <fren.ankit@gmail.com>
2016-01-08 18:19:47 +0545

    Improve app startup configuration, add News feed fragment

--- android/app/src/main/java/com/lipi/notifica/MainActivity.java
+++ android/app/src/main/java/com/lipi/notifica/MainActivity.java
@@ -1,6 +1,9 @@
 package com.lipi.notifica;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,13 +17,14 @@ import android.widget.Toast;
 import com.lipi.notifica.database.Client;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MyActivity";
     private Toolbar toolbar;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
-    //Button test;
 
+    private RoutineFragment mRoutineFragment;
+    private NewsfeedFragment mNewsfeedFragment;
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
 
     @Override
@@ -31,21 +35,18 @@ public class MainActivity extends AppCompatActivity {
         // Download and get new routine
         Client client = new Client(this);
         client.getRoutine(new Client.ClientListener() {
+            // The refresh is called number of times as new data are downloaded.
+            // This.queue represents number of refresh callbacks that are pending.
+            // When it is zero, it means everything is downloaded completely.
             @Override
             public void refresh() {
-                // The refresh is called number of times as new data are downloaded.
-                // This.queue represents number of refresh callbacks that are pending.
-                // When it is zero, it means everything is downloaded completely.
                 Log.d("refreshing routine", "queue size: "+this.queue.size());
             }
         });
 
-        /*FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.frame_content, new RoutineFragment()).commitAllowingStateLoss();*/
-
-       /* test  = (Button) findViewById(R.id.test);
-        test.setText("Test");*/
+        // Create the fragment once
+        mRoutineFragment = new RoutineFragment();
+        mNewsfeedFragment = new NewsfeedFragment();
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
@@ -59,80 +60,60 @@ public class MainActivity extends AppCompatActivity {
             // This method will trigger on item Click of navigation menu
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-
-                //Checking if the item is in checked state or not, if not make it in checked state
-                if(menuItem.isChecked()) menuItem.setChecked(false);
-                else menuItem.setChecked(true);
-
-                //Closing drawer on item click
-                drawerLayout.closeDrawers();
-
-                //Check to see which item was being clicked and perform appropriate action
+                Fragment selectedFragment = null;
                 switch (menuItem.getItemId()){
-
-
-                    //Replacing the main content with RoutineFragment
-                    case R.id.routine:
-                        Toast.makeText(getApplicationContext(), "Routine Selected", Toast.LENGTH_SHORT).show();
-                        RoutineFragment fragment = new RoutineFragment();
-                        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        fragmentTransaction.replace(R.id.frame_content,fragment);
-                        fragmentTransaction.commit();
-                        return true;
-
-                    // For rest of the options we just show a toast on click
-
-                    case R.id.assignment:
-                        Toast.makeText(getApplicationContext(),"Assignment",Toast.LENGTH_SHORT).show();
-                        return true;
                     case R.id.news_feed:
-                        Toast.makeText(getApplicationContext(),"News Feed",Toast.LENGTH_SHORT).show();
-                        return true;
+                        selectedFragment = mNewsfeedFragment;
+                        break;
+                    case R.id.routine:
+                        selectedFragment = mRoutineFragment;
+                        break;
+                    case R.id.assignment:
+                        break;
                     case R.id.settings:
-                        Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
-                        return true;
+                        break;
                     default:
-                        Toast.makeText(getApplicationContext(),"Somethings Wrong",Toast.LENGTH_SHORT).show();
-                        return true;
-
+                        break;
                 }
+                if( selectedFragment != null ){
+                    android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_content, selectedFragment);
+                    fragmentTransaction.commit();
+                    if(menuItem.isChecked()) menuItem.setChecked(false);
+                    else menuItem.setChecked(true);
+                    drawerLayout.closeDrawers();
+                }
+                return true;
             }
         });
 
         // Initializing Drawer Layout and ActionBarToggle
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
-                super.onDrawerClosed(drawerView);
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
-
-                super.onDrawerOpened(drawerView);
-            }
-        };
+        mDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer);
 
         //Setting the actionbarToggle to drawer layout
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        drawerLayout.setDrawerListener(mDrawerToggle);
 
-        //calling sync state is necessay or else your hamburger icon wont show up
-        actionBarDrawerToggle.syncState();
-
-
-
+        if (savedInstanceState == null) {
+            // start up app with News feed
+            android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.frame_content, mNewsfeedFragment);
+            fragmentTransaction.commit();
+            navigationView.getMenu().findItem(R.id.news_feed).setChecked(true);
+        }
     }
-/*
-    public void Press(View v) {
-        Intent i = new Intent(this, SubjectDetailsActivity.class);
-        startActivity(i);
-        Log.d(TAG, "Press: zzz");
-    }
-*/
 
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 }
