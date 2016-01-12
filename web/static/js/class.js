a2c4ac29295eadf4819d4eb11fb234386ed61781
Frozen Helium <fren.ankit@gmail.com>
2016-01-12 17:26:43 +0545

    design basic class view

---
+++ web/static/js/class.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('body').on( 'click', '.tab', function(e){
+        var current = $('#tabs .active');
+        var target = $(this);
+        var target_id = target.data('target');
+        var current_id = current.data('target');
+        if(target_id != current_id){
+            var current_container = $('#tab-contents #'+ current_id);
+            var target_container = $('#tab-contents #'+ target_id);
+            current.removeClass('active');
+            target.addClass('active');
+            current_container.removeClass('active');
+            current_container.addClass('hidden');
+            target_container.removeClass('hidden');
+            target_container.addClass('active');
+        }
+    });
+});
