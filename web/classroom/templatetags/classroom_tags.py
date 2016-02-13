4ab116706e395f7d69e71196b42717ad9a318198
bibek <bibekdahal.bd16@gmail.com>
2016-02-13 20:44:51 +0545

    Send time in iso from django in Post

--- web/classroom/templatetags/classroom_tags.py
+++ web/classroom/templatetags/classroom_tags.py
@@ -2,6 +2,7 @@ from django import template
 from classroom.utils import *
 
 import json
+from datetime import datetime
 
 register = template.Library()
 
@@ -9,3 +10,8 @@ register = template.Library()
 @register.filter
 def json_decode(string):
     return json.loads(string)
+
+
+@register.filter
+def to_iso(date):
+    return date.isoformat(' ')
